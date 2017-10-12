96e8697728ba2a7e069c08665625b6dbb4f13dcb
Alfredo Deza <adeza@redhat.com>
2017-10-12 16:08:52 -0400

    ceph-volume util.arg_validators allow to pass a group as --data for lvm
    Signed-off-by: Alfredo Deza <adeza@redhat.com>

--- src/ceph-volume/ceph_volume/util/arg_validators.py
+++ src/ceph-volume/ceph_volume/util/arg_validators.py
@@ -1,4 +1,5 @@
 import argparse
+from ceph_volume.api import lvm
 
 
 class LVPath(object):
@@ -9,10 +10,25 @@ class LVPath(object):
 
     Because for LVM it is better to be specific on what group does an lv
     belongs to.
+
+    Allow a vg group to be defined as well, since it is possible that a user
+    may want to pass that so that lvm can carve out an LV.
     """
 
     def __call__(self, string):
         error = None
+        if '/' not in string:
+            # it is possible we have a volume group
+            vg = lvm.get_vg(vg_name=string)
+            if not vg:
+                error = (
+                    "Argument is not a volume group (allowed for bluestore) or a device. "
+                    "Devices must be an absolute path. "
+                    "For filestore argument must be volume_group/logical_volume"
+                )
+                raise argparse.ArgumentError(None, error)
+            else:
+                return string
         try:
             vg, lv = string.split('/')
         except ValueError:
